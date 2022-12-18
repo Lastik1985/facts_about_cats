b180abb56dc4893f55f840240d8c56ffc7dfb8cd
Lastik1985 <113602670+Lastik1985@users.noreply.github.com>
2022-12-18 21:19:57 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 
